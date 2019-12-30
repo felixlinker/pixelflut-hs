9eba3f53528c2d63add89aa4503e3565c1df09b5
felixlinker <linkerfelix@gmail.com>
2019-12-30 16:28:47 +0100

    Randomize drawing

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,22 +10,46 @@ import           Codec.Picture.Types
 import           Control.Exception
 import           Text.Bytedump
 import           Text.Printf
--- import           System.Random
--- import           System.Random.Shuffle
+import           System.Random
+import           System.Random.Shuffle
 import qualified Data.ByteString.Char8         as B
+import qualified Data.Map                      as Map
 
-data PX = PX Int Int String
+data PX = PX
+    { x :: Int
+    , y :: Int
+    , hex :: String }
 instance Show PX where
     show (PX x y hex) = printf "PX %d %d %s\n" x y hex
 
 shift :: Int -> Int -> PX -> PX
 shift dx dy (PX x y c) = PX (x + dx) (y + dy) c
 
+randomize :: [PX] -> [PX]
+-- Initialize mkStdGen with 0 because true randomness is not important
+randomize ps = shuffle' ps (length ps) $ mkStdGen 0
+
+countColors :: Fractional frc => [PX] -> Map.Map String frc
+countColors = foldl (\acc v -> Map.insertWith (+) (hex v) 1 acc) Map.empty
+
+relMap :: (Ord ord, Fractional frc) => Map.Map ord frc -> Map.Map ord frc
+relMap m = let sum = Map.foldl (+) 0 m in fmap (/ sum) m
+
+inflate :: [PX] -> [PX]
+inflate ps =
+    let colorRelFreq = (relMap . countColors) ps
+    in  concat $ map
+            (\(PX x y hex) ->
+                let freq = colorRelFreq Map.! hex
+                in  replicate (ceiling $ 1 / freq) (PX x y hex)
+            )
+            ps
+
 rgb8ToHex :: Pixel8 -> Pixel8 -> Pixel8 -> String
 rgb8ToHex r g b = (foldl1 (++) . map hexString) [r, g, b]
 
 payloads :: Int -> Int -> B.ByteString -> [B.ByteString]
-payloads dx dy = map (B.pack . show . shift dx dy) . pxs
+payloads dx dy = map (B.pack . show . shift dx dy) . randomize . pxs
 
 data E = E String
 instance Show E where
